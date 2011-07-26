887501f7b5d1a59f4690613a1b78b116b003dfab
claudia <cayamagt@msn.com>
2011-07-26 10:13:51 -0600

    Finished the password machinery

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                 :integer         not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
+#
 class User < ActiveRecord::Base
   attr_accessor    :password
   attr_accessible :name, :email, :password, :password_confirmation
@@ -12,17 +23,40 @@ class User < ActiveRecord::Base
   validates :password, :presence => true,
                        :confirmation => true,
                        :length => { :within => 6..40 }
+
+  before_save :encrypt_password
+
+  def has_password?(submitted_password)
+    encrypted_password == encrypt(submitted_password)
+  end
+
+  def User.authenticate(email, submitted_password)
+    user = find_by_email(email) # primero se busca el usuario x email
+    # user = User.find_by_email(email) sintáxis equivalente
+    return nil if user.nil? # regresa nulo si no lo encuentra
+    return user if user.has_password?(submitted_password) # regresa el usuario que tenga ese password
+  end
+
+  private
+
+    def encrypt_password
+      self.salt = make_salt if new_record? #cuando se asigna se usa self.
+      self.encrypted_password = encrypt(password)
+    end
+
+    def encrypt(string)
+      secure_hash("#{salt}--#{string}")
+    end
+
+    def make_salt
+      secure_hash("#{Time.now.utc}--#{password}")
+    end
+
+    def secure_hash(string)
+      Digest::SHA2.hexdigest(string)
+    end
+
 end
 
-# == Schema Information
-#
-# Table name: users
-#
-#  id                 :integer         not null, primary key
-#  name               :string(255)
-#  email              :string(255)
-#  created_at         :datetime
-#  updated_at         :datetime
-#  encrypted_password :string(255)
-#
+
 
